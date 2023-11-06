787c69352bf92a91686d63c3ee2841d80ef4c8d0
Maciel Meireles <132109923+macielmeireles@users.noreply.github.com>
2023-11-06 18:11:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
 
-# Variáveis
+# Variables - Review and modify as needed
 glpi_folder="/www/site/glpi"
+bot_token="your_token_bot"
+chat_id="your_chat_id"
+
 glpi_version="10.0.10"
 github_url="https://raw.githubusercontent.com/macielmeireles/glpi_sendTelegramLogin/main"
-original_login_file="https://raw.githubusercontent.com/glpi-project/glpi/$glpi_version/bugfixes/front/login.php"
-bot_token="seu_token_do_bot"
-chat_id="seu_chat_id"
+original_login_file="https://raw.githubusercontent.com/glpi-project/glpi/10.0/bugfixes/front/login.php"
 
-# Navega para a pasta do GLPI
+# Navigate to GLPI directory
 cd $glpi_folder
 
-# Verifica a versão do GLPI
-current_version=$(php -r "include 'inc/config.php'; echo substr(trim(GLPI_VERSION), 0, 6);")
+# Check GLPI version
+current_version=$(grep "define('GLPI_VERSION'" inc/define.php | cut -d ',' -f 2 | tr -d " ');\r")
 if [ "$current_version" != "$glpi_version" ]; then
-    echo "A versão atual do GLPI é $current_version, mas a versão necessária é $glpi_version. Interrompendo a instalação."
+    echo "Current GLPI version is $current_version, but required version is $glpi_version. Aborting installation."
     exit 1
 fi
 
-# Verifica se a opção --restore foi fornecida
+# Check if --restore option is provided
 if [ "$1" == "--restore" ]; then
-    curl -o front/login.php $original_login_file
-    echo "Arquivo login.php restaurado"
+    curl -o $glpi_folder/front/login.php $original_login_file
+    echo ""
+    echo ""
+    echo "original login.php file restored"
+    echo ""
     exit 0
 fi
 
-# Baixa os arquivos do GitHub
+# Download files from GitHub
 for file in login_mod.php login_new.php sendTelegram.sh; do
     curl -O $github_url/$file
 done
 
-# Faz backup do arquivo login.php local
-mv front/login.php login_bak_local.php
+# Backup local login.php file
+mv $glpi_folder/front/login.php $glpi_folder/login_bak_local.php
 
-# Move os novos arquivos para a pasta correta
-mv login_mod.php front/login_mod.php 
-mv login_new.php front/login.php
-mv sendTelegram.sh ../sendTelegram.sh
+# Move new files to correct directory
+mv login_mod.php $glpi_folder/front/login_mod.php 
+mv login_new.php $glpi_folder/front/login.php
+mv sendTelegram.sh $glpi_folder/../sendTelegram.sh
 
-echo "Fim"
+# Grant execute permission to sendTelegram.sh file
+chmod +x $glpi_folder/../sendTelegram.sh
 
+# Replace variables in sendTelegram.sh file
+sed -i "s/your_token/$bot_token/g" $glpi_folder/../sendTelegram.sh
+sed -i "s/your_chat_id/$chat_id/g" $glpi_folder/../sendTelegram.sh
+
+echo " "
+echo " "
+echo "Installation complete!"
+echo " "
+echo "To restore, use: ./install --restore"
+echo " "
