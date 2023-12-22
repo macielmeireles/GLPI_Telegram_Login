7bdf7ac3eb1c4ae10c882edee5d9148fb2dfea7c
Maciel Meireles <132109923+macielmeireles@users.noreply.github.com>
2023-12-22 20:01:17 -0300

    Update new_front_login.php

--- new_front_login.php
+++ new_front_login.php
@@ -1,6 +1,6 @@
 <?php
-if (file_exists(__DIR__ . '/login_mod.php')) {
-    include(__DIR__ . '/login_mod.php');
+if (file_exists(__DIR__ . '/custom_front_login_with_telegram.php')) {
+    include(__DIR__ . '/custom_front_login_with_telegram.php');
 } else {
-    include(__DIR__ . '/login_orig.php');
+    include(__DIR__ . '/login_bak_local.php');
 }
